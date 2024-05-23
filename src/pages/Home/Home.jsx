5b7216794f395e17248254982a72217f8546db7c
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-23 14:25:20 +0600

    Homepage Layout And Date Time Formatting Using Moment.Js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
+import Header from "../shared/header/Header";
+import LeftSideNav from "../shared/leftsidenav/LeftSideNav";
+import Navbar from "../shared/navbar/Navbar";
+import RightSideNav from "../shared/rightsidenav/RightSideNav";
 
 
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This is Home</h2>
+            <Header></Header>
+            <Navbar></Navbar>
+            <div className="border grid grid-cols-1 lg:grid-cols-4 gap-6">
+                <div className=" border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className=" border text-3xl md:col-span-2">News coming soon..</div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
