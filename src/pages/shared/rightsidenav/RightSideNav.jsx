8ee85824abfca1df4c6dfc0ec6807db566ae030d
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-23 16:28:33 +0600

    Right Navigation And Info With Icons

--- src/pages/shared/rightsidenav/RightSideNav.jsx
+++ src/pages/shared/rightsidenav/RightSideNav.jsx
@@ -1,8 +1,45 @@
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import qzone1 from '../../../assets/qZone1.png'
+import qzone2 from '../../../assets/qZone2.png'
+import qzone3 from '../../../assets/qZone3.png'
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-2xl">Right Side Nav</h2>
+            <div className='p-4 space-y-4 mb-6'>
+                <h2 className="text-3xl">Login with</h2>
+                <button className="btn btn-outline w-full">
+                    <FaGoogle></FaGoogle>
+                    Google
+                </button>
+                <button className="btn btn-outline w-full">
+                    <FaGithub></FaGithub>
+                    Github
+                </button>
+            </div>
+            <div className='p-4 mb-6'>
+                <h2 className="text-3xl mb-4">Find us</h2>
+                <a className='flex p-4 text-lg items-center border rounded-t-lg' href="">
+                    <FaFacebook className='mr-3'></FaFacebook>
+                    Facebook
+                </a>
+                <a className='flex p-4 text-lg items-center border-x' href="">
+                    <FaTwitter className='mr-3'></FaTwitter>
+                    Twitter
+                </a>
+                <a className='flex p-4 text-lg items-center border rounded-b-lg' href="">
+                    <FaInstagram className='mr-3'></FaInstagram>
+                    Instagram
+                </a>
+            </div>
+            {/* Q-zone */}
+            <div className='p-4 space-y-4 mb-6'>
+                <h2 className="text-3xl">Q Zone</h2>
+                <img src={qzone1} alt="" />
+                <img src={qzone2} alt="" />
+                <img src={qzone3} alt="" />
+                
+            </div>
         </div>
     );
 };
