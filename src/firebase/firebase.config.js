c9bcadfa0f6bce5f7a805a212de7f0f1e3a446a4
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-23 11:52:47 +0600

    project stup:raect router,tailwind,daisyui,firbase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAag-bbAQM5fjhaatBqnUmSuIN8z_CLlA4",
+  authDomain: "daily-news-today-3d5a7.firebaseapp.com",
+  projectId: "daily-news-today-3d5a7",
+  storageBucket: "daily-news-today-3d5a7.appspot.com",
+  messagingSenderId: "762878124473",
+  appId: "1:762878124473:web:ed266bdd1b538fc3055668"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
